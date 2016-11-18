7d7fa0986bf01e68db876e99cc9dec4698828bf6
Yoshiya Hinosawa <stibium121@gmail.com>
2016-11-18 22:56:32 +0900

    add example of mock

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 const axios = require('axios')
 
+window = { alert () {} }
+
 exports.getGithubProfile = function (name) {
   return axios.get(`https://api.github.com/users/${name}`)
+}
+
+exports.checkMoney = function (money, price) {
+  if (money < price) {
+    window.alert('残金が足りません')
+  }
 }
--- test.js
+++ test.js
@@ -2,7 +2,9 @@ const axios = require('axios')
 const sinon = require('sinon')
 const td = require('testdouble')
 const assert = require('assert')
-const { getGithubProfile } = require('./')
+const { getGithubProfile, checkMoney } = require('./')
+
+window = { alert: () => {} }
 
 describe('getGithubProfile', () => {
   it('与えられたユーザの github profile を取得する', done => {
@@ -32,5 +34,49 @@ describe('getGithubProfile', () => {
 
       done()
     })
+  })
+})
+
+describe('checkMoney @td', () => {
+  beforeEach(() => {
+    td.replace(window, 'alert')
+  })
+
+  afterEach(() => {
+    td.reset()
+  })
+
+  it('残金が足りない場合は警告ダイアログを表示', () => {
+    checkMoney(500, 1000)
+
+    td.verify(window.alert('残金が足りません'))
+  })
+
+  it('残金が十分ある場合は警告ダイアログを表示しない', () => {
+    checkMoney(1500, 1000)
+
+    assert.throws(() => td.verify(window.alert('残金が足りません')))
+  })
+})
+
+describe('checkMoney @sinon', () => {
+  it('残金が足りない場合は警告ダイアログを表示', () => {
+    const mock = sinon.mock(window).expects('alert').withArgs('残金が足りません')
+
+    checkMoney(500, 1000)
+
+    mock.verify()
+
+    window.alert.restore()
+  })
+
+  it('残金が十分ある場合は警告ダイアログを表示しない', () => {
+    const mock = sinon.mock(window).expects('alert').withArgs('残金が足りません').never()
+
+    checkMoney(1500, 1000)
+
+    mock.verify()
+
+    window.alert.restore()
   })
 })
